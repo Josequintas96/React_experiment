cf886a6d5b334ba1f045223963e0e80ecd8d915a
Jose Manuel Quintas <failop@Joses-MacBook-Pro.local>
2021-01-11 20:02:04 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {render} from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+//import {Router, Route} from "react-router";
+
+import ReactX from "./Componets/React.js";
+import Mirror from "./Componets/Mirror.js";
+import Mirror2 from "./Componets/Mirror2.js";
+import Buttons from "./Componets/Buttons.js";
+import Calculations from "./Componets/Calculations.js";
+import Square from "./Componets/Square.js";
+//import { AuthProvider } from './components/Auth';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ //   <AuthProvider>
+      <Router>
+      <div className="App_Body" >
+              <div className='AppB'>              
+                <Switch>
+                    <Route path="/" exact component={ReactX} />
+                    <Route path="/Mirror" component={Mirror} />
+                    <Route path="/Mirror2" component={Mirror2} />
+                    <Route path="/React" component={ReactX} />
+                    
+                    <Route path="/Buttons" component={Buttons} />
+                    <Route path="/Calculations" component={Calculations} />
+                    <Route path="/Square" component={Square} />
+                    {/* <Route path="/Login" render={(props) => (<Login Input={this.state.username} Input2={this.state.password} Input3 = {this.Login_in} /> )} /> */}
+              </Switch>
+            </div>  
+          </div>
+      </Router>
+ //   </AuthProvider>
+
   );
 }
 
---
+++ src/Componets/Square.js
@@ -0,0 +1,56 @@
+import logo from '../logo.svg';
+import '../css/core.css';
+import Pok from './Experiment/pok'
+
+function Square() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="CoreX">
+        &nbsp;
+        1st
+                <div className='CoreX0'>
+                        <Pok/>
+                </div>
+        
+            <p> </p>
+
+        2nd
+                <div className='CoreX0'>
+                        <Pok/>
+                </div>
+
+            <p> </p>
+
+        3rd
+                <div className='CoreX0'>
+                        <Pok/>
+                </div>
+
+            <p> </p>
+
+        4th
+                <div className='CoreX0'>
+                        <Pok/>
+                </div>
+
+        <p> </p>
+
+        5th
+                <div className='CoreX0'>
+                        <Pok/>
+                </div>
+        <p> </p>
+
+
+       </div>
+
+
+          Learn Square
+
+      </header>
+    </div>
+  );
+}
+
+export default Square;
