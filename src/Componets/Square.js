5b42e8cd5b44b4b922b83574838dbd9604c4c0a6
Jose Manuel Quintas <failop@Joses-MacBook-Pro.local>
2021-01-11 20:29:22 -0500

    Update Square.js

--- src/Componets/Square.js
+++ src/Componets/Square.js
@@ -1,56 +1,77 @@
-import logo from '../logo.svg';
+import React from "react";
 import '../css/core.css';
+
 import Pok from './Experiment/pok'
+import Pok_r from './Experiment/pok2_r'
+import Pok_rr from './Experiment/pok2_rr'
+import Pok_g from './Experiment/pok2_g'
+import Pok_y from './Experiment/pok2_y'
 
-function Square() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="CoreX">
-        &nbsp;
-        1st
-                <div className='CoreX0'>
-                        <Pok/>
-                </div>
-        
-            <p> </p>
+class Square extends React.Component
+{
 
-        2nd
-                <div className='CoreX0'>
-                        <Pok/>
-                </div>
+        constructor(props) 
+        {
+                super(props);
+                this.state = { username: '' };
+        };
+        render()
+        {
 
-            <p> </p>
+        return (
+        <div className="App">
+                <header className="App-header">
+                        <div className="CoreX">
+                                &nbsp;
+                                1st
+                                <div className='CoreX0'>
+                                        <Pok_g/>
+                                </div>
+                
+                                <p> </p>
 
-        3rd
-                <div className='CoreX0'>
-                        <Pok/>
-                </div>
+                                2nd
+                                <div className='CoreX0'>
+                                        <Pok_rr/>
+                                </div>
 
-            <p> </p>
+                                <p> </p>
 
-        4th
-                <div className='CoreX0'>
-                        <Pok/>
-                </div>
+                                3rd
+                                <div className='CoreX0'>
+                                        <Pok_r/>
+                                </div>
 
-        <p> </p>
+                                <p> </p>
 
-        5th
-                <div className='CoreX0'>
-                        <Pok/>
-                </div>
-        <p> </p>
+                                4th
+                                <div className='CoreX0'>
+                                        <Pok_y/>
+                                </div>
+
+                                <p> </p>
+
+                                5th
+                                <div className='CoreX0'>
+                                        <Pok/>
+                                </div>
+                                <p> </p>
 
 
-       </div>
+                                6th
+                                <div className='CoreX0'>
+                                        <Pok/>
+                                </div>
+                                <p> </p>
+
+                        </div>
 
 
-          Learn Square
-
-      </header>
-    </div>
+                Learn Square
+                </header>
+        </div>
   );
+    }
 }
 
 export default Square;
